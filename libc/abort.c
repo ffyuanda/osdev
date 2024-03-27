de094d7b99146cacfa3c697f1031bcd42bc93007
Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>
2024-03-27 16:36:43 -0700

    overhaul: flatten libc/ directory
    Overhaul is a series of changes that aim to bring the whole project into
    a flattened directory. The motivation is to cut away unnecessary mental
    burden created by nested directories. This is not to say that
    directories are useless, but they should only be added when it's
    absolutely beneficial/necessary. Before that, we can keep things simple
    in a single, flattened directory. This is a modification based on the
    Meaty Skeleton template provided at osdev.
    
    This commit is the second step, which flattened the libc/ directory.
    The next step is bringing everything into the same root directory.
    
    Signed-off-by: Shaoxuan Yuan <shaoxuan.yuan02@gmail.com>

--- libc/abort.c
+++ libc/abort.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
 __attribute__((__noreturn__))
 void abort(void) {
--- libc/itoa.c
+++ libc/itoa.c
@@ -1,4 +1,4 @@
-#include <string.h>
+#include "string.h"
 #include <stdbool.h>
 
 /* A utility function to reverse a string */
